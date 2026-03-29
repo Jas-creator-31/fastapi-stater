ea2f7d39c5af7f1358bbbe6229cd6db2b40068aa
jashan <jas123hanpreet@gmail.com>
2026-03-29 17:52:25 +0530

    main.py setup

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
-from fastapi
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+origins = [
+  "https://ledgeless-solvolytic-jesenia.ngrok-free.dev",
+  "https://localhost",
+  "https://192.168.31.190",
+  "https://tijuana-unsymbolical-miss.ngrok-free.dev",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          # List of allowed origins
+    allow_credentials=True,         # Allow cookies/authorization headers (requires specific origins)
+    allow_methods=["*"],            # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],            # Allow all HTTP request headers
+)
