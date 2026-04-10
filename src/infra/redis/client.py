0e46103f7629ea3ecbe504b34ab39bfec1984d87
jashan <jas123hanpreet@gmail.com>
2026-04-10 17:28:01 +0530

    did some formating in client.py of redis

--- src/infra/redis/client.py
+++ src/infra/redis/client.py
@@ -8,12 +8,13 @@ redis_host = os.getenv("REDIS_HOST")
 redis_port = os.getenv("REDIS_PORT")
 redis_db = os.getenv("REDIS_DB")
 
+
 async def get_redis():
     r = redis.Redis(
-        host=redis_host, # type: ignore
-        port=int(redis_port), # type: ignore
-        db=int(redis_db) # type: ignore
-        ) 
+        host=redis_host,  # type: ignore
+        port=int(redis_port),  # type: ignore
+        db=int(redis_db),  # type: ignore
+    )
     try:
         yield r
     finally:
